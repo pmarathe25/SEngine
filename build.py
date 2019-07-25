06c74017e459fd680c652c2202daa87f45046c9f
Pranav Marathe <pmarathe25@gmail.com>
2019-07-24 20:00:05 -0700

    Updates for SLog changes

--- build.py
+++ build.py
@@ -5,6 +5,6 @@ import glob
 project = sbuildr.Project()
 
 for source in glob.iglob("tests/*.cpp"):
-    project.test(source.split('.')[0], sources=[source], libs=["stdc++", "stest", "slog"])
+    project.test(source.split('.')[0], sources=[source], libs=["stdc++", "stest"])
 
 sbuildr.cli(project, default_profiles=["debug"])
